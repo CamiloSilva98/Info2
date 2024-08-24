d347ee14f88af243c2f8b59fb5d52a501a3dd794
CamiloSilva98 <36543134+CamiloSilva98@users.noreply.github.com>
2024-08-24 11:13:55 -0500

    Update main.cpp

--- info2/Problema6/Problema6/main.cpp
+++ info2/Problema6/Problema6/main.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 int main()
 {
-    int num, fact=1;
-    cout<<"Ingrese un numero: ";
+    int num;
+    long long int fact=1;
+    cout<<"Ingrese un numero entre 1 y 65: ";
     cin>>num;
-    float div, e=0;
+    while(num>65)
+        {
+            cout<<"Ingrese un numero entre 1 y 65: ";
+            cin>>num;
+        }
+    float div, e=1;
     for(int i=1;i<=num;i++)
     {
+        fact=fact*i;
         div=1.0/fact;
         e=e+div;
-        fact=fact*i;
     }
     cout<<e;
     return 0;
